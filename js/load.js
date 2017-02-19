6f9b74ed668122a4f52db0a4f06d3f4b62415714
EnigmaSn <enigmasnape@gmail.com>
2017-02-19 22:41:58 +0300

    1 задание

--- js/load.js
+++ js/load.js
@@ -1,13 +1,22 @@
-window.load = function () {
+window.load = function (url, onload) {
   var xhr = new XMLHttpRequest();
 
-  // Конфигурируем его: GET-запрос на URL 'phones.json'
-  xhr.open('GET', 'file.json');
-  xhr.send();
+  // Конфигурируем запрос: GET-запрос на URL
+  // третий параметр - асинхронность (true по умолчанию)
+  xhr.open('GET', 'url');
+
+  // обработчик всегда до отправки запроса
+  xhr.addEventListener('load', function(evt) {
+    // onLoad — функция обратного вызова, которая срабатывает при успешном выполнении запроса. При вызове функции onLoad в её единственный параметр передается набор полученных данных.
+    onLoad(evt.target.response);
+  });
 
   if (xhr.status != 200) {
-    alert ('Алярм, там ошибка, хозяин!');
+    console.log('Насяльнике, чет не так. Вот, сам посмотри: '+ xhr.status + ': ' + xhr.statusText);
   } else {
-    alert( xhr.responseText ); // responseText -- текст ответа.
+    console.log(xhr.responseText); // responseText -- текст ответа.
   }
+
+  xhr.send(); // отправляем запрос
+  // у GET запросов нет тела
 };
