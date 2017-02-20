0c80b6b538e262b919e27f10de8481522d2551e0
EnigmaSn <enigmasnape@gmail.com>
2017-02-20 14:34:46 +0300

    ошибки еслинта

--- js/load.js
+++ js/load.js
@@ -1,3 +1,5 @@
+'use strict';
+
 window.load = function (url, onLoad) {
   var xhr = new XMLHttpRequest();
 
@@ -6,20 +8,14 @@ window.load = function (url, onLoad) {
   xhr.open('GET', 'url');
 
   // обработчик всегда до отправки запроса
-  xhr.addEventListener('load', function(evt) {
+  xhr.addEventListener('load', function (evt) {
     // onLoad — функция обратного вызова, которая срабатывает при успешном выполнении запроса. При вызове функции onLoad в её единственный параметр передается набор полученных данных.
-    if (evt.target.status = 200) {
+    if (evt.target.status === 200) {
       var data = JSON.parse(evt.target.response);
       onLoad(data);
     }
 
   });
-
-  if (xhr.status != 200) {
-    console.log('Насяльнике, чет не так. Вот, сам посмотри: '+ xhr.status + ': ' + xhr.statusText);
-  } else {
-    console.log(xhr.responseText); // responseText -- текст ответа.
-  }
 
   xhr.send(); // отправляем запрос
   // у GET запросов нет тела
