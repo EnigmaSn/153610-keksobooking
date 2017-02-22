a407971a977d0a63d13d01a377b1cd6e147f5256
EnigmaSn <enigmasnape@gmail.com>
2017-02-22 15:09:42 +0300

    data в showDialog

--- js/initialize-pins.js
+++ js/initialize-pins.js
@@ -4,11 +4,9 @@ window.initializePins = function () {
   var ENTER_KEY_CODE = 13;
 
   var pinMap = document.querySelector('.tokyo__pin-map'); // обертка для пинов
+
   window.similarApartments = null;
-  window.load('https://intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data', function (data) {
-    window.similarApartments = data;
-    window.renderPins(3, pinMap); // вызываю функцию отрисовки нового пина: 3 штуки в соответствующую обертку
-  });
+
   // деактивация пина при переключении
   var disableActive = function () {
     var element = document.querySelector('.pin--active');
@@ -29,15 +27,28 @@ window.initializePins = function () {
     activePin.focus();
   };
 
-  // вызов
+  window.load('https://intensive-javascript-server-pedmyactpq.now.sh/keksobooking/data', function (data) {
+    window.similarApartments = data;
+    window.renderPins(3, pinMap); // вызываю функцию отрисовки нового пина: 3 штуки в соответствующую обертку
+  });
+
+  // Добавьте каждому из отрисованных пинов обработчик события клика (и активации с клавиатуры), который будет вызывать функцию показа карточки, передавая в нее данные соответствующего объявления.
 
   // нажатие на пины через делегирование
   pinMap.addEventListener('click', function (event) {
-    // не только клик по пину, но и внутри него
-    if (event.target.closest('.pin')) {
+    var index = event.target.closest('.pin').getAttribut('data-index'); // индекс нажатого пина
+    var data = window.similarApartments[index];
+    // Лезешь в data по этому индексу
+    if (index) {
       onDialogShow(event);
-      window.showCard(onDialogClose, similarApartments);
+      window.showCard(onDialogClose, data);
     }
+
+    // не только клик по пину, но и внутри него
+    // if (event.target.closest('.pin')) {
+    //   onDialogShow(event);
+    //   window.showCard(onDialogClose, similarApartments);
+    // }
   });
 
   pinMap.addEventListener('keydown', function (event) {
